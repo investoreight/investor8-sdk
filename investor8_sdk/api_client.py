760dae3bd44b7ad68f32caf3e71398003e70a350
Mehrdad Rafiei <mehrdadrafiei@outlook.com>
2022-08-31 22:34:32 +0200

    Merge branch 'fix/update-the-missing-logic-of-get-current-metrics-v2-api' into 'master'
    Update the missing logic of get current metrics v2 API
    
    See merge request babakloni/investor8.core!400

--- investor8_sdk/api_client.py
+++ investor8_sdk/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.1.44/python'
+        self.user_agent = 'Swagger-Codegen/1.1.45/python'
 
     def __del__(self):
         self.pool.close()
