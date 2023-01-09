bc3647ec107013c34f2f59b3b3f92baf80162d9a
Mehrdad Rafiei <mehrdadrafiei@outlook.com>
2023-01-09 08:57:47 +0100

    Merge branch 'feature/update-screen-search-api' into 'master'
    Update screen search API
    
    See merge request babakloni/investor8.core!418

--- investor8_sdk/api_client.py
+++ investor8_sdk/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.1.64/python'
+        self.user_agent = 'Swagger-Codegen/1.1.65/python'
 
     def __del__(self):
         self.pool.close()
