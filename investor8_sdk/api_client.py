e39135e49d544299092dda0cad3a41aa27df5e97
Mehrdad Rafiei <mehrdadrafiei@outlook.com>
2023-05-14 16:14:02 +0200

    Merge branch 'feature/update-screener-search-api-to-return-active-tickers' into 'master'
    Update the screener search API to return active tickers
    
    See merge request babakloni/investor8.core!435

--- investor8_sdk/api_client.py
+++ investor8_sdk/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.1.79/python'
+        self.user_agent = 'Swagger-Codegen/1.1.80/python'
 
     def __del__(self):
         self.pool.close()
