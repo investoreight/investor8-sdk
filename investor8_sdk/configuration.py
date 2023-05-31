9d2d633feebe45d96cabcabc4dcd80f10544fd8d
Mehrdad Rafiei <mehrdadrafiei@outlook.com>
2023-05-31 09:45:46 +0200

    Merge branch 'fix/screener-search-api-bug' into 'master'
    Fix the ScreenerSearch API bug
    
    See merge request babakloni/investor8.core!443

--- investor8_sdk/configuration.py
+++ investor8_sdk/configuration.py
@@ -254,5 +254,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.1\n"\
-               "SDK Package Version: 1.1.85".\
+               "SDK Package Version: 1.1.86".\
                format(env=sys.platform, pyversion=sys.version)
