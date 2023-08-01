674dcc3ce2efe96f0bdaf653bf7242d2734b1d82
Mehrdad Rafiei <mehrdadrafiei@outlook.com>
2023-08-01 18:26:46 +0200

    Merge branch 'fix/edit-screening-profile-bug' into 'master'
    Fix the EditScreeningProfile bug
    
    See merge request babakloni/investor8.core!469

--- investor8_sdk/api_client.py
+++ investor8_sdk/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.1.106/python'
+        self.user_agent = 'Swagger-Codegen/1.1.107/python'
 
     def __del__(self):
         self.pool.close()
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "investor8-sdk"
-VERSION = "1.1.106"
+VERSION = "1.1.107"
 # To install the library, run the following
 #
 # python setup.py install
